35e556bbee7da7f18afb75c49fba7b4bb9b19c96
gonza2206 <gonzalopez2206@gmail.com>
2023-05-24 16:43:24 -0300

    feat: Create new LedModeSerializable-BatteryStatus
    Signed-off-by: gonza2206 <gonzalopez2206@gmail.com>

---
+++ src/LedModeSerializable-BatteryStatus.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "LedModeSerializable.h"
+#include "cstdio"
+
+#ifdef KEYSCANNER
+#include "LEDManagement.hpp"
+#endif
+
+class LedModeSerializable_BatteryStatus : public LedModeSerializable {
+ public:
+  explicit LedModeSerializable_BatteryStatus(uint32_t id)
+    : LedModeSerializable(id) {
+  }
+  uint8_t serialize(uint8_t *output) const override {
+    uint8_t index   = LedModeSerializable::serialize(output);
+    output[index]   = r_;
+    output[++index] = g_;
+    output[++index] = b_;
+    output[++index] = w_;
+    return ++index;
+  }
+
+  uint8_t deSerialize(const uint8_t *input) override {
+    uint8_t index          = LedModeSerializable::deSerialize(input);
+    r_                     = input[index];
+    g_                     = input[++index];
+    b_                     = input[++index];
+    w_                     = input[++index];
+    base_settings.delay_ms = 100;
+    return ++index;
+  }
+#ifdef NEURON_WIRED
+  void update() override {
+  }
+#endif
+
+#ifdef KEYSCANNER
+  void update() override {
+    LEDManagement::set_all_leds({r_, g_, b_, w_});
+    printf("red color %d", r_);
+    LEDManagement::set_updated(true);
+  }
+#endif
+  uint8_t r_, g_, b_, w_;
+
+ private:
+  u_int8_t batteryStatus;
+  uint16_t rainbowHue        = 0;
+  uint16_t rainbowSaturation = 255;
+  uint8_t rainbowLastUpdate  = 0;
+};
+
+static LedModeSerializable_BatteryStatus ledModeSerializableBatteryStatus{CRC32_STR("LedModeSerializable_BatteryStatus")};
