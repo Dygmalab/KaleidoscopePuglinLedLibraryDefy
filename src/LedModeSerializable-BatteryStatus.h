98d7e7c18701f252aa1a735158e5e15c6cffc248
gonza2206 <gonzalopez2206@gmail.com>
2023-07-26 13:56:59 -0300

    fix: battery bugs related-
    Co-authored-by: Gustavo Gomez Lopez De San Roman <gustavogomezlopezdesanroman@gmail.com>
    Signed-off-by: gonza2206 <gonzalopez2206@gmail.com>

--- src/LedModeSerializable-BatteryStatus.h
+++ src/LedModeSerializable-BatteryStatus.h
@@ -30,7 +30,6 @@ class LedModeSerializable_BatteryStatus : public LedModeSerializable {
 #ifdef KEYSCANNER
 
   void update() override {
-    BatteryManagement::resetSleepTimer();
     RGBW first_cell, second_cell, fourth_cell, third_cell = {0, 0, 0, 0};
     
     uint8_t batteryLevel = BatteryManagement::getBatteryLevel();
@@ -101,7 +100,6 @@ class LedModeSerializable_BatteryStatus : public LedModeSerializable {
     LEDManagement::set_led_at(first_cell, 6);
     LEDManagement::set_led_at(second_cell, 13);
     LEDManagement::set_led_at(third_cell, 20);
-    BatteryManagement::releaseMutex();
   }
 
  private:
