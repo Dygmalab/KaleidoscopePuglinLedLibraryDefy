b814627293ae53f1447bd8dfd6ca62297a13e0dc
gonza2206 <gonzalopez2206@gmail.com>
2023-08-14 11:56:09 -0300

    chore: set control variables
    Signed-off-by: gonza2206 <gonzalopez2206@gmail.com>

--- src/LedModeSerializable-BluetoothPairing.h
+++ src/LedModeSerializable-BluetoothPairing.h
@@ -14,41 +14,33 @@ class LedModeSerializable_BluetoothPairing : public LedModeSerializable {
   }
   uint8_t serialize(uint8_t *output) const override {
     uint8_t index   = LedModeSerializable::serialize(output);
-    output[index]   = r_;
-    output[++index] = g_;
-    output[++index] = b_;
-    output[++index] = w_;
+    output[index]   = paired_channels_;
+    output[++index] = connected_channel_id_;
+    output[++index] = paired_channel_id_;
     return ++index;
   }
 
   uint8_t deSerialize(const uint8_t *input) override {
     uint8_t index          = LedModeSerializable::deSerialize(input);
-    r_                     = input[index];
-    g_                     = input[++index];
-    b_                     = input[++index];
-    w_                     = input[++index];
+    paired_channels_                     = input[index];
+    connected_channel_id_                     = input[++index];
+    paired_channel_id_                     = input[++index];
     base_settings.delay_ms = 100;
     return ++index;
   }
-#ifdef NEURON_WIRED
-  void update() override {
-    kaleidoscope::Runtime.device().ledDriver().setCrgbNeuron({r_, g_, b_, w_});
-  }
-#endif
 
 #ifdef KEYSCANNER
   void update() override {
-    LEDManagement::set_all_leds({r_, g_, b_, w_});
+    LEDManagement::set_all_leds({255, 255, 255, 255});
     LEDManagement::set_updated(true);
   }
 #endif
-  uint8_t r_, g_, b_, w_;
+  uint8_t paired_channels_;
+  uint8_t connected_channel_id_;
+  uint8_t paired_channel_id_;
 
  private:
-  uint16_t rainbowHue        = 0;
-  uint16_t rainbowSaturation = 255;
-  uint8_t rainbowLastUpdate  = 0;
 };
 
 static LedModeSerializable_BluetoothPairing
-  LedModeSerializable_BluetoothPairing{CRC32_STR("LedModeSerializable_BluetoothPairing")};
+  ledModeSerializableBluetoothPairing{CRC32_STR("LedModeSerializable_BluetoothPairing")};
