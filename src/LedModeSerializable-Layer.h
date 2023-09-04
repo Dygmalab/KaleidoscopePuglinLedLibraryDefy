f7d6896af854e843d226ea5a54e7b15825aca34d
gonza2206 <gonzalopez2206@gmail.com>
2023-09-04 17:01:22 +0200

    feat: brightness decrease when user change from one layer to another.
    Signed-off-by: gonza2206 <gonzalopez2206@gmail.com>

--- src/LedModeSerializable-Layer.h
+++ src/LedModeSerializable-Layer.h
@@ -2,6 +2,7 @@
 
 #include "LedModeSerializable.h"
 #include "cstdio"
+#include "debug_print.h"
 
 class LedModeSerializable_Layer : public LedModeSerializable {
  public:
@@ -16,6 +17,7 @@ class LedModeSerializable_Layer : public LedModeSerializable {
 
   uint8_t deSerialize(const uint8_t *input) override {
     uint8_t index = LedModeSerializable::deSerialize(input);
+    base_settings.delay_ms = 100;
     layer         = input[index];
     return ++index;
   }
@@ -26,18 +28,48 @@ class LedModeSerializable_Layer : public LedModeSerializable {
 
 #ifdef KEYSCANNER
   void update() override {
+    static float led_driver_brightness = LEDManagement::get_max_ledDriver_brightness();
+    static float underglow_brightness = LEDManagement::get_max_underglow_brightness();
+    static bool flag = true;
+    static RGBW aux_pallete;
+
     LEDManagement::Layer actualLayer{};
     if (this->layer < LEDManagement::layers.size()) {
       actualLayer = LEDManagement::layers.at(this->layer);
     }
+    if (layer != previous_layer){
+      DBG_PRINTF_TRACE("layer: %i", layer);
+      DBG_PRINTF_TRACE("previous_layer: %i", previous_layer);
+      previous_layer = layer;
+    }
+    //TODO: Fijate que el maximo brillo para el BL y el UG son distintos
     for (uint8_t i = 0; i < NUMBER_OF_LEDS; i++) {
       RGBW &color = LEDManagement::palette[actualLayer.leds[i]];
       LEDManagement::set_led_at(color, i);
     }
+    if (layer != 0){
+      led_driver_brightness = led_driver_brightness - 0.02;
+      underglow_brightness = underglow_brightness -  0.03;
+    } else {
+      led_driver_brightness = led_driver_brightness + 0.02;
+      underglow_brightness = underglow_brightness  + 0.03;
+    }
+    DBG_PRINTF_TRACE("led_driver_brightness: %f", led_driver_brightness);
+    DBG_PRINTF_TRACE("underglow_brightness: %f", underglow_brightness);
+    if (led_driver_brightness <= 0.10){
+      base_settings.delay_ms = 0;
+    } else if (led_driver_brightness > LEDManagement::get_max_ledDriver_brightness()){
+      led_driver_brightness  = LEDManagement::get_max_ledDriver_brightness();
+      underglow_brightness = LEDManagement::get_max_underglow_brightness();
+      base_settings.delay_ms = 0;
+    }
+    LEDManagement::set_ledDriver_brightness(led_driver_brightness);
+    LEDManagement::set_underglow_brightness(led_driver_brightness);
     LEDManagement::set_updated(true);
   }
 #endif
   uint8_t layer;
+  uint8_t previous_layer = 0xff;
 
  private:
 };
