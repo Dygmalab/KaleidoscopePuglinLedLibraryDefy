480e5d567613eb2f918f49bba3fe92294e06ef36
gonza2206 <gonzalopez2206@gmail.com>
2023-10-18 09:37:35 -0300

    feat: change low battery effect. Discharge animation.

--- src/LedModeSerializable-LowBattery.h
+++ src/LedModeSerializable-LowBattery.h
@@ -30,37 +30,52 @@ class LedModeSerializable_LowBattery : public LedModeSerializable {
 #ifdef KEYSCANNER
 
   void update() override {
-    static uint32_t lastExecutionTime = 0;
-    static bool blinking = false;
-    uint32_t currentTime = to_ms_since_boot(get_absolute_time());
-
-    DBG_PRINTF_TRACE("counter: %i", counter);
-
-    if (counter > 0) {
-      if (currentTime - lastExecutionTime >= 73) {
-        blinking = !blinking;
-
-        if (blinking) {
-          LEDManagement::set_all_leds(red);
-        } else {
-          LEDManagement::set_all_leds(ledOff);
-          counter--;
-        }
-
-        lastExecutionTime = currentTime;
-      }
-    } else {
-      LEDManagement::set_all_leds(ledOff);
+    RGBW first_cell, second_cell, third_cell = {0, 0, 0, 0};
+    /*Column effect*/
+    static enum {
+      FIRST_CELL,
+      SECOND_CELL,
+      THIRD_CELL,
+      NO_CELL,
+    } currentCell = NO_CELL;
+    switch (currentCell) {
+    case NO_CELL:
+      first_cell  = red;
+      second_cell = red;
+      third_cell  = red;
+      currentCell = THIRD_CELL;
+      break;
+    case THIRD_CELL:
+      first_cell  = ledOff;
+      second_cell = red;
+      third_cell  = red;
+      currentCell = SECOND_CELL;
+      break;
+    case SECOND_CELL:
+      first_cell  = ledOff;
+      second_cell = ledOff;
+      third_cell  = red;
+      currentCell = FIRST_CELL;
+      break;
+    case FIRST_CELL:
+      first_cell  = ledOff;
+      second_cell = ledOff;
+      third_cell  = ledOff;
+      currentCell = NO_CELL;
+      break;
     }
-
+    counter--;
+    LEDManagement::set_led_at(first_cell, 6);
+    LEDManagement::set_led_at(second_cell, 13);
+    LEDManagement::set_led_at(third_cell, 20);
     LEDManagement::set_updated(true);
   }
 
   void resetCounter() {
-    counter = 5;
+    counter = 9;
   }
  public:
-  uint8_t counter = 5;
+  uint8_t counter = 3;
  private:
   static constexpr RGBW red    = {255, 0, 0, 0};
   static constexpr RGBW ledOff = {0, 0, 0, 0};
