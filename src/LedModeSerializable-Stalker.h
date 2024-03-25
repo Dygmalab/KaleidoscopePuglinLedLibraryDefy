aab541b9313574397154893f228dc181e00b407f
gonza2206 <gonzalopez2206@gmail.com>
2024-03-25 17:52:01 -0300

    fix: Led effects for Raise 2.

--- src/LedModeSerializable-Stalker.h
+++ src/LedModeSerializable-Stalker.h
@@ -128,7 +128,16 @@ class LedModeSerializable_Stalker : public LedModeSerializable {
 
 
         #ifdef RAISE2
-          uint8_t pos = i * Pins::COLS + j;
+        uint8_t led_position;
+          //uint8_t pos = i * Pins::COLS + j;
+        if (gpio_get(Pins::SIDE_ID)) {
+          //Right side.
+           led_position = pos_right[i][j];
+        } else {
+          //Left side.
+           led_position = pos_left[i][j];
+        }
+
         #else
         uint8_t pos = i * (Pins::ROWS + 2) + j;
         if (pos > 26) {
@@ -138,10 +147,10 @@ class LedModeSerializable_Stalker : public LedModeSerializable {
 
         if (step) {
           RGBW color = compute_function_(step);
-          LEDManagement::set_led_at(color, pos);
+          LEDManagement::set_led_at(color, led_position);
         }
         if (!step) {
-          LEDManagement::set_led_at({}, pos);
+          LEDManagement::set_led_at({}, led_position);
         }
       }
     }
@@ -164,6 +173,23 @@ class LedModeSerializable_Stalker : public LedModeSerializable {
  private:
 #ifdef KEYSCANNER
   std::function<RGBW(uint8_t &)> compute_function_;
+
+  uint8_t pos_left[Pins::ROWS][Pins::COLS] = {
+    {0,1, 2, 3,4,5,6},
+    {7,8, 9, 10,11,12},
+    {13,14, 15, 16,17,18},
+    {19,20, 21, 22,23,24,25},
+    {26,27, 28, 29,30,31,32,33},
+  };
+
+  uint8_t pos_right[Pins::ROWS][Pins::COLS] = {
+    {0,1, 2, 3,4,5,6},
+    {15,8, 9, 10,11,12,13,14},
+    {7,16, 17, 18,19,20,21},
+    {22,23, 24, 25,26,27},
+    {28,29, 30, 31,32,33,34,35},
+  };
+
 #endif
 };
 
